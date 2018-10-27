8dea67d1c51d6b7219956d6951a08191ad9b5234
tim <timothy.tran835@gmail.com>
2018-10-26 19:52:29 -0700

    got DB to pass down fake datas

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ProductDetails from './ProductDetails';
-import Specifications from './Specifications';
 import Recommendations from './Recommendations';
 import RecentlyViewed from './RecentlyViewed';
 import OthersBought from './OthersBought';
@@ -9,17 +8,24 @@ import OthersBought from './OthersBought';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      products: [],
+    };
   }
 
   componentDidMount() {
-    this.fetchData;
+    this.fetchData();
   }
 
   fetchData() {
     axios
       .get('shoedidas/product/details')
-      .then(data => res.status(200).send(data))
+      .then(data => {
+        let products = data.data;
+        this.setState({
+          products,
+        });
+      })
       .catch(err => console.error(err));
   }
 
