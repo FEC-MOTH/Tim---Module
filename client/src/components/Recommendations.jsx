aa5723511fec4c708892bda250f01bbb32948c2a
tim <timothy.tran835@gmail.com>
2018-11-01 11:54:46 -0700

    refactored recommended to stateful component. Will likely do the same for the others

--- client/src/components/Recommendations.jsx
+++ client/src/components/Recommendations.jsx
@@ -1,55 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import style from '../css/Recommendations.css';
 import SvgStar from '../components/starSvg.jsx';
 import ReactSVG from 'react-svg';
 
-const Recommendations = function(props) {
-  const images = props.products.image;
-  const imagesArr = JSON.parse(images);
+export default class Recommendations extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: 'hello',
+    };
+  }
 
-  const price = props.products.price.toString();
+  render() {
+    console.log(this.props);
+    const images = this.props.products.image;
+    const imagesArr = JSON.parse(images);
 
-  const correctPrice = function(str) {
-    return str.length === 5 ? '$' + str.slice(0, 3) : '$' + str.slice(0, 2);
-  };
+    const price = this.props.products.price.toString();
 
-  return (
-    <div className="hockeyCard">
-      <span className="productContainer">
-        <div className="productCard">
-          <div className="productInfo">
-            <img
-              className="img"
-              src={`http://demandware.edgesuite.net/sits_pod20-adidas/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/${
-                imagesArr[0].id
-              }/zoom/${imagesArr[0].fileName}?sh=1024`}
-            />
-            <div className="favorite">
-              <div className="toggleWishList">
-                <img
-                  className="icon"
-                  src={
-                    props.boolean
-                      ? 'https://png.icons8.com/metro/1600/hearts.png'
-                      : 'https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png'
-                  }
-                  onClick={props.toggle}
-                />
+    const correctPrice = function(str) {
+      return str.length === 5 ? '$' + str.slice(0, 3) : '$' + str.slice(0, 2);
+    };
+
+    const randomRating = () => Math.floor(Math.random() * 100);
+    return (
+      <div className="hockeyCard">
+        <span className="productContainer">
+          <div className="productCard">
+            <div className="productInfo">
+              <img
+                className="img"
+                src={`http://demandware.edgesuite.net/sits_pod20-adidas/dw/image/v2/aaqx_prd/on/demandware.static/-/Sites-adidas-products/en_US/${
+                  imagesArr[0].id
+                }/zoom/${imagesArr[0].fileName}?sh=1024`}
+              />
+              <div className="favorite">
+                <div className="toggleWishList">
+                  <img
+                    className="icon"
+                    src={
+                      this.props.boolean
+                        ? 'https://png.icons8.com/metro/1600/hearts.png'
+                        : 'https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/heart-outline-512.png'
+                    }
+                    onClick={this.props.toggle}
+                  />
+                </div>
+              </div>
+              <div className="productDetails">
+                <div className="productName">{this.props.products.item_name}</div>
+                <div className="productPrice">{correctPrice(price)}</div>
+              </div>
+              <div className="rating">
+                <SvgStar />
+                <SvgStar />
+                <SvgStar />
+                <SvgStar />
+                <SvgStar />
+                <div className="numbersOfRaters">{randomRating()}</div>
               </div>
             </div>
-            <div className="productDetails">
-              <div className="productName">{props.products.item_name}</div>
-              <div className="productPrice">{correctPrice(price)}</div>
-            </div>
-            <div className="rating">
-              {/* <SvgStar /> */}
-              <div className="noRated" />
-            </div>
           </div>
-        </div>
-      </span>
-    </div>
-  );
-};
-
-export default Recommendations;
+        </span>
+      </div>
+    );
+  }
+}
--- client/src/components/starSvg.jsx
+++ client/src/components/starSvg.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import style from '../css/starSvg.css';
 
 const SvgStar = function(props) {
   return (
-    <svg class="gl-star-rating__star" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
+    <svg className="gl-star-rating__star" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
       <linearGradient id="05d80a5f-9258-49d4-b43b-b4ca7883263b">
-        <stop offset="0%" stop-opacity="1" style="stop-color: currentcolor;" />
-        <stop offset="100%" stop-opacity="1" style="stop-color: currentcolor;" />
-        <stop offset="100%" stop-opacity="0" style="stop-color: currentcolor;" />
-        <stop offset="100%" stop-opacity="0" style="stop-color: currentcolor;" />
+        <stop offset="0%" stopOpacity="1" style={{ stopColor: 'currentcolor' }} />
+        <stop offset="100%" stopOpacity="1" style={{ stopColor: 'currentcolor' }} />
+        <stop offset="100%" stopOpacity="0" style={{ stopColor: 'currentcolor' }} />
+        <stop offset="100%" stopOpacity="0" style={{ stopColor: 'currentcolor' }} />
       </linearGradient>
       <path
         fill="none"
         stroke="currentColor"
-        stroke-miterlimit="10"
+        strokeMiterlimit="10"
         d="M13.277,6.182L9.697,8.782L11.057,12.992L7.487,10.392L3.907,12.992L5.277,8.782L1.697,6.182L6.117,6.182L7.487,1.992L8.857,6.182L13.277,6.182Z"
       />
       <path
